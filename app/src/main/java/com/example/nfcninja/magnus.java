b9f8c25d1499bbab5b78df5e28d1a967b719431e
paragoncode <magnus.laursen00@gmail.com>
2025-04-26 13:45:36 +0200

    UI changes and onClick activity

--- app/src/main/java/com/example/nfcninja/magnus.java
+++ app/src/main/java/com/example/nfcninja/magnus.java
@@ -1,6 +1,11 @@
 package com.example.nfcninja;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +25,22 @@ public class magnus extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-// test
+        Spinner spinnerCard=findViewById(R.id.spinnerCard);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.recent_cards, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerCard.setAdapter(adapter);
+
+        Button addCard = (Button) findViewById(R.id.newCardButton);
+        addCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence text = "Scan card";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(magnus.this, text, duration);
+                toast.show();
+            }
+        });
     }
 }
