4eb2ba6065f93dd18cb2a3a3b778ba18a980b5c0
paragoncode <magnus.laursen00@gmail.com>
2025-04-26 12:18:10 +0200

    commit test

--- app/src/main/java/com/example/nfcninja/magnus.java
+++ app/src/main/java/com/example/nfcninja/magnus.java
@@ -20,5 +20,6 @@ public class magnus extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+// test
     }
 }
