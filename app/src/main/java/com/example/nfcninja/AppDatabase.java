dbc2e5cc5193ce063e5a1dc72cf0c3d71091b38d
paragoncode <magnus.laursen00@gmail.com>
2025-04-26 11:58:19 +0200

    Revert "Database Done?"
    This reverts commit d35b3d1712b0fda6e95ff2675e1a1bce946825c6.

--- app/src/main/java/com/example/nfcninja/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.nfcninja;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {NfcaTag.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract NfcaTagDao nfcaTagDao();
-}
